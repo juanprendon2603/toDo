22a17e0ebf6e5211cd5a443baee8586a93283088
Juan Rendon <juanprendon2603@gmail.com>
2024-01-15 21:32:16 -0500

    configuracion inicial firebase y primera pantalla de base

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:to_do/screens/task_list_screen.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const TaskListScreen(),
     );
   }
 }
--- lib/screens/task_list_screen.dart
+++ lib/screens/task_list_screen.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
+import '../models/task_model.dart';
 
+class TaskListScreen extends StatelessWidget {
+  const TaskListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Task> tasks = [
+      Task(
+          title: 'Tarea 1',
+          description: 'Descripción de la tarea 1',
+          isCompleted: true,
+          date: DateTime(2024)),
+      Task(
+          title: 'Tarea 2',
+          description: 'Descripción de la tarea 2',
+          isCompleted: false,
+          date: DateTime(2024)),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ToDO List'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          Task task = tasks[index];
+          return ListTile(
+            title: Text(task.title),
+            subtitle: Text(task.description),
+            trailing: Checkbox(
+              value: false,
+              onChanged: (value) {},
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
